8313e192f733423ef0e68285f57903c3f00b4678
Censurados <osvaldosouza53@outlook.com>
2022-05-09 21:29:15 -0300

    funçao no menu mk

---
+++ script.js
@@ -0,0 +1,6 @@
+function choose() {
+    const mk = document.getElementById('menumk')
+    mk.innerText = 'CHOOSE YOUR DESTINY...'
+    mk.style.fontFamily = 'MK'
+    mk.style.fontWeight = '500'
+}
