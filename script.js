0e5073ee8c10bd09bbef64ecc27627b34f7199aa
Censurados <osvaldosouza53@outlook.com>
2022-06-24 20:15:17 -0300

    adicionei contador brigas
    adicionado contador de dias sem brigas censuradas narradas

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function choose() {
     mk.style.fontFamily = 'MK'
     mk.style.fontWeight = '500'
 }
+
+let hoje = new Date()
+let ultimoVideo = new Date("05/11/2022") // inserir aqui data do ultimo brigas postado, formato: mes-dia-ano
+const diasSemVideo = (hoje - ultimoVideo) / 86400000 //milissegundos dia
+let contadorBrigas = document.getElementById('contador-brigas-narradas')
+contadorBrigas.innerHTML = `ATENÇÃO! Estamos há ${parseInt(diasSemVideo)} dias sem brigas censuradas...`
+
